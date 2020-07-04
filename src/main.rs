dc92666c9428d5056e40c18b1e997e100de3cb6f
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-07-04 23:53:20 +0300

    Added dependencies for a HTTP server and client

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-mod auth;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 
-fn main() {
-    println!("Hello, world!");
+
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello, world")))
+}
+
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let server = Server::bind(&addr).serve(make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle))
+    }));
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
