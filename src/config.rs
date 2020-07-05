ea8e4e9eeb61d56318ea6a436d5775bbd263c837
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-07-05 18:34:14 +0300

    Implemented parsing fixed size hexadecimal strings

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use http::uri::Uri;
 use clap::{App, Arg, ArgMatches};
+use generic_array::{GenericArray, ArrayLength};
 
 
 pub fn parse_options<'a>() -> ArgMatches<'a>{
@@ -65,53 +66,120 @@ pub fn parse_wiki_uri(uri: &str) -> Result<Uri, String> {
 }
 
 
+fn parse_hex_string<N: ArrayLength<u8>>(value: &str) -> Result<GenericArray<u8, N>, String> {
+    let mut result = GenericArray::<u8, N>::default();
+    let expected_length = result.len() * 2;
+
+    if value.len() < expected_length {
+        return Err(format!("String is too short, {} hex digits expected", expected_length))
+    }else if value.len() > expected_length {
+        return Err(format!("String is too long, {} hex digits expected", expected_length))
+    }
+
+    for (i, c) in value.chars().enumerate() {
+        match c.to_digit(16) {
+            Some(digit) => result[i / 2] = result[i / 2] << 4 | (digit as u8),
+            None =>  return Err(format!("Invalid character at position {}", i + 1))
+        }
+    }
+    Ok(result)
+}
+
 
 #[cfg(test)]
 mod tests {
-    use super::parse_wiki_uri;
+    mod test_parsing_uri {
+        use super::super::parse_wiki_uri;
 
-    #[test]
-    fn test_invalid_uri(){
-        assert_eq!(
-            parse_wiki_uri("http::wrong-uri"),
-            Err(String::from("Cannot parse url: http::wrong-uri"))
-        );
+        #[test]
+        fn test_invalid_uri(){
+            assert_eq!(
+                parse_wiki_uri("http::wrong-uri"),
+                Err(String::from("Cannot parse url: http::wrong-uri"))
+            );
+        }
+
+        #[test]
+        fn test_invalid_protocol(){
+            assert_eq!(
+                parse_wiki_uri("ftp://localhost:7000/path"),
+                Err(String::from("Protocol not supported: ftp"))
+            );
+        }
+
+        #[test]
+        fn test_correct_uri(){
+            assert_eq!(
+                parse_wiki_uri("http://localhost:5000/path"),
+                Ok("http://localhost:5000/path".parse().unwrap())
+            );
+        }
+
+        #[test]
+        fn test_implied_schema_and_query(){
+            assert_eq!(
+                parse_wiki_uri("localhost:12345"),
+                Ok("http://localhost:12345/".parse().unwrap())
+            );
+        }
+
+        #[test]
+        fn test_missing_authority(){
+            assert_eq!(parse_wiki_uri("/path"), Err(String::from("Missing authority")));
+        }
+
+        #[test]
+        fn test_illegal_query(){
+            assert_eq!(
+                parse_wiki_uri("http://localhost/?query"),
+                Err(String::from("URL cannot contain a query"))
+            );
+        }
     }
 
-    #[test]
-    fn test_invalid_protocol(){
-        assert_eq!(
-            parse_wiki_uri("ftp://localhost:7000/path"),
-            Err(String::from("Protocol not supported: ftp"))
-        );
-    }
+    mod test_parsing_hex {
+        use hex_literal::hex;
+        use generic_array::typenum::U10;
+        use super::super::parse_hex_string;
 
-    #[test]
-    fn test_correct_uri(){
-        assert_eq!(
-            parse_wiki_uri("http://localhost:5000/path"),
-            Ok("http://localhost:5000/path".parse().unwrap())
-        );
-    }
+        #[test]
+        fn test_string_too_short(){
+            assert_eq!(
+                parse_hex_string::<U10>(&"112233445566778899"),
+                Err(String::from("String is too short, 20 hex digits expected"))
+            );
+        }
 
-    #[test]
-    fn test_implied_schema_and_query(){
-        assert_eq!(
-            parse_wiki_uri("localhost:12345"),
-            Ok("http://localhost:12345/".parse().unwrap())
-        );
-    }
+        #[test]
+        fn test_string_too_long(){
+            assert_eq!(
+                parse_hex_string::<U10>(&"11223344556677889900AA"),
+                Err(String::from("String is too long, 20 hex digits expected"))
+            );
+        }
 
-    #[test]
-    fn test_missing_authority(){
-        assert_eq!(parse_wiki_uri("/path"), Err(String::from("Missing authority")));
-    }
+        #[test]
+        fn test_invalid_character(){
+            assert_eq!(
+                parse_hex_string::<U10>(&"112233~4556677889900"),
+                Err(String::from("Invalid character at position 7"))
+            );
+        }
 
-    #[test]
-    fn test_illegal_query(){
-        assert_eq!(
-            parse_wiki_uri("http://localhost/?query"),
-            Err(String::from("URL cannot contain a query"))
-        );
+        #[test]
+        fn test_correct_lowercase(){
+            match parse_hex_string::<U10>(&"0123456789abcdef0123") {
+                Ok(result) => assert_eq!(result[..], hex!("0123456789abcdef0123")),
+                Err(_) => assert!(false)
+            }
+        }
+
+        #[test]
+        fn test_correct_uppercase(){
+            match parse_hex_string::<U10>(&"0123456789ABCDEF0123") {
+                Ok(result) => assert_eq!(result[..], hex!("0123456789abcdef0123")),
+                Err(_) => assert!(false)
+            }
+        }
     }
 }
