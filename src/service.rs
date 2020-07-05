faaf5ad672b986ad413ae31e7f8fdc6dfc7c65b0
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-07-05 19:53:44 +0300

    Created tests for checking conditional proxy behaviour

--- src/service.rs
+++ src/service.rs
@@ -52,79 +52,149 @@ pub async fn handle(request: Request<Body>, config: Arc<ProxyConfig>) -> Respons
 
 #[cfg(test)]
 mod tests {
-    use std::time::SystemTime;
-    use hyper::Request;
-    use super::is_authenitcated;
-    use crate::auth::Token;
-    use crate::auth::tests::MockConfig;
+    mod test_is_authenticated {
+        use std::time::SystemTime;
+        use hyper::Request;
+        use super::super::is_authenitcated;
+        use crate::auth::Token;
+        use crate::auth::tests::MockConfig;
 
-    #[test]
-    fn test_auth_no_cookies() {
-        let request = Request::builder().body(()).unwrap();
-        let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
-        assert!(!is_authenitcated(&request, &config));
+        #[test]
+        fn test_auth_no_cookies() {
+            let request = Request::builder().body(()).unwrap();
+            let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+            assert!(!is_authenitcated(&request, &config));
+        }
+
+        #[test]
+        fn test_auth_no_invalid_cookie_syntax() {
+            let request = Request::builder()
+                .header("Cookie", "invalid-value")
+                .body(())
+                .unwrap();
+
+            let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+            assert!(!is_authenitcated(&request, &config));
+        }
+
+        #[test]
+        fn test_auth_no_such_cookie() {
+            let request = Request::builder()
+                .header("Cookie", "cookie1=2; cookie2=3")
+                .body(())
+                .unwrap();
+
+            let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+            assert!(!is_authenitcated(&request, &config));
+        }
+
+        #[test]
+        fn test_auth_wrong_cookie_value() {
+            let request = Request::builder()
+                .header("Cookie", "cookie1=2; proxy_auth=invalid_value; cookie2=3")
+                .body(())
+                .unwrap();
+
+            let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+            assert!(!is_authenitcated(&request, &config));
+        }
+
+        #[test]
+        fn test_auth_expired_token() {
+            let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+            let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+
+            let request = Request::builder()
+                .header("Cookie", format!(
+                    "cookie1=2; proxy_auth={}; cookie2=3",
+                    Token::new(now - 100).generate(&config)
+                ))
+                .body(())
+                .unwrap();
+            assert!(!is_authenitcated(&request, &config));
+        }
+
+        #[test]
+        fn test_auth_valid_token() {
+            let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+            let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+
+            let request = Request::builder()
+                .header("Cookie", format!(
+                    "cookie1=2; proxy_auth={}; cookie2=3",
+                    Token::new(now + 100).generate(&config)
+                ))
+                .body(())
+                .unwrap();
+            assert!(is_authenitcated(&request, &config));
+        }
     }
 
-    #[test]
-    fn test_auth_no_invalid_cookie_syntax() {
-        let request = Request::builder()
-            .header("Cookie", "invalid-value")
-            .body(())
-            .unwrap();
+    mod test_navigation {
+        use std::sync::Arc;
+        use http::Uri;
+        use httpmock::{Mock, MockServer};
+        use hyper::{Request, Body};
+        use crate::config::ProxyConfig;
+        use crate::auth::Token;
+        use super::super::handle;
+        use std::time::SystemTime;
+        use futures::stream::StreamExt;
 
-        let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
-        assert!(!is_authenitcated(&request, &config));
-    }
+        #[tokio::test]
+        async fn test_redirecting_unauthenticated_to_login_page(){
+            let mock_server = MockServer::start();
+            let config = ProxyConfig::from_values(
+                &format!("{}", mock_server.address()),
+                "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF"
+            ).unwrap();
 
-    #[test]
-    fn test_auth_no_such_cookie() {
-        let request = Request::builder()
-            .header("Cookie", "cookie1=2; cookie2=3")
-            .body(())
-            .unwrap();
+            let mock = Mock::new()
+                .expect_method(httpmock::Method::GET)
+                .expect_path("/hello")
+                .create_on(&mock_server);
 
-        let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
-        assert!(!is_authenitcated(&request, &config));
-    }
+            let request = Request::builder()
+                .uri("/hello".parse::<Uri>().unwrap())
+                .method("GET")
+                .body(Body::empty()).unwrap();
 
-    #[test]
-    fn test_auth_wrong_cookie_value() {
-        let request = Request::builder()
-            .header("Cookie", "cookie1=2; proxy_auth=invalid_value; cookie2=3")
-            .body(())
-            .unwrap();
+            let resp = handle(request, Arc::new(config)).await;
+            assert_eq!(resp.status(), 307);
+            assert_eq!(resp.headers().get("Location").unwrap(), "/");
+            assert_eq!(mock.times_called(), 0);
+        }
 
-        let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
-        assert!(!is_authenitcated(&request, &config));
-    }
+        #[tokio::test]
+        async fn test_running_proxy_when_authenticated(){
+            let mock_server = MockServer::start();
+            let config = ProxyConfig::from_values(
+                &format!("{}", mock_server.address()),
+                "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF"
+            ).unwrap();
 
-    #[test]
-    fn test_auth_expired_token() {
-        let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-        let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+            let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+            let token = Token::new(now + 100).generate(&config);
 
-        let request = Request::builder()
-            .header("Cookie", format!(
-                "cookie1=2; proxy_auth={}; cookie2=3",
-                Token::new(now - 100).generate(&config)
-            ))
-            .body(())
-            .unwrap();
-        assert!(!is_authenitcated(&request, &config));
-    }
+            let mock = Mock::new()
+                .expect_method(httpmock::Method::GET)
+                .expect_path("/hello")
+                .return_body("remote content")
+                .create_on(&mock_server);
 
-    #[test]
-    fn test_auth_valid_token() {
-        let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-        let config = MockConfig::new(*b"00112233445566778899AABBCCDDEEFF");
+            let request = Request::builder()
+                .uri("/hello".parse::<Uri>().unwrap())
+                .method("GET")
+                .header("Cookie", format!("proxy_auth={}", token))
+                .body(Body::empty()).unwrap();
 
-        let request = Request::builder()
-            .header("Cookie", format!(
-                "cookie1=2; proxy_auth={}; cookie2=3",
-                Token::new(now + 100).generate(&config)
-            ))
-            .body(())
-            .unwrap();
-        assert!(is_authenitcated(&request, &config));
+            let resp = handle(request, Arc::new(config)).await;
+            assert_eq!(resp.status(), 200);
+            let body = String::from_utf8(resp.into_body()
+                .map(|c| c.unwrap().to_vec())
+                .concat().await).unwrap();
+            assert_eq!(body, "remote content");
+            assert_eq!(mock.times_called(), 1);
+        }
     }
 }
